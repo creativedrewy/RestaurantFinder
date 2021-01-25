a0ef0f5c6eec3ae0b8d38e4ef0156a7cae6fb806
Andrew Watson <creativedrewy@gmail.com>
2021-01-25 14:35:22 -0800

    Get more data back from the API

--- app/src/main/java/com/creativedrewy/restaurantfinder/api/ApiData.kt
+++ app/src/main/java/com/creativedrewy/restaurantfinder/api/ApiData.kt
@@ -2,5 +2,19 @@ package com.creativedrewy.restaurantfinder.api
 
 data class RestaurantListDto(
     val numResults: Int,
-    val nextOffset: Int
+    val nextOffset: Int,
+    val stores: List<RestaurantDto>
+)
+
+data class RestaurantDto(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val coverImgUrl: String,
+    val status: RestaurantStatusDto
+)
+
+data class RestaurantStatusDto(
+    val unavailableReason: String?,
+    val asapMinutesRange: List<Int>
 )
