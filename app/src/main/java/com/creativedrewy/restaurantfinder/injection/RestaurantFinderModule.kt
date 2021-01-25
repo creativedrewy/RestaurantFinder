f0582a94464abc477c90bab1213f0969f8880282
Andrew Watson <creativedrewy@gmail.com>
2021-01-25 14:28:26 -0800

    Properly inject GSON so that we can have a naming policy.

--- app/src/main/java/com/creativedrewy/restaurantfinder/injection/RestaurantFinderModule.kt
+++ app/src/main/java/com/creativedrewy/restaurantfinder/injection/RestaurantFinderModule.kt
@@ -2,22 +2,35 @@ package com.creativedrewy.restaurantfinder.injection
 
 import com.creativedrewy.restaurantfinder.BuildConfig
 import com.creativedrewy.restaurantfinder.api.RestaurantsEndpoints
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 class RestaurantFinderModule {
 
+    @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideGson(): Gson {
+        return GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.RESTAURANTS_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 
