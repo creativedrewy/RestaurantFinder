a2ba165072fe5f5fcdc0f00c8cc391016ae56a9e
Andrew Watson <creativedrewy@gmail.com>
2021-03-01 16:20:37 -0800

    Add login button, viewmodel

--- app/src/main/java/com/creativedrewy/restaurantfinder/LoginFragment.kt
+++ app/src/main/java/com/creativedrewy/restaurantfinder/LoginFragment.kt
@@ -5,18 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.creativedrewy.restaurantfinder.databinding.FragmentLoginBinding
+import com.creativedrewy.restaurantfinder.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  *
  */
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var viewBinding: FragmentLoginBinding
+
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        viewBinding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return viewBinding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewBinding.loginButton.setOnClickListener {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/creativedrewy/restaurantfinder/viewmodel/LoginViewModel.kt
@@ -0,0 +1,10 @@
+package com.creativedrewy.restaurantfinder.viewmodel
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel @ViewModelInject constructor(
+
+): ViewModel() {
+
+}
